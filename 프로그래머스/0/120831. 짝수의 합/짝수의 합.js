3557e35d838a3be08635cd628e16e06df82f1794
서지호 <164027964+Seo-JH-08@users.noreply.github.com>
2024-07-05 08:30:16 +0900

    Update 짝수의 합.js

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,5 +1,6 @@
 function solution(n) {
     let answer = 0
+    //초기 값을 짝수 중 가장 작은 값인 2부터(0은 더해도 의미가 없으니 ..) 시작해 i가 n 이하일때 계속 2씩 증가, i를 answer 변수에 계속 더해준다
     for(let i = 2; i <= n ; i+=2) answer+=i
     return answer;
 }
